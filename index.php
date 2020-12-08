d1a5d7ba560c6b32cd690c6b69307947277ef894
Max <maxblanck@outlook.fr>
2020-12-08 14:47:14 +0100

    php Partie 2  Exercice 8

--- index.php
+++ index.php
@@ -1,23 +1,25 @@
-<?php 
+<?php
 $isOk = 0;
 ?>
 
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
+
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Exercice 8 Partie 2</title>
 </head>
+
 <body>
     <h1>Exercice 8 Partie 2</h1>
-    <?php 
-    if ($isOk) {
-        echo 'c\'est ok !!';
-    } else {
-        echo 'c\'est pas bon !!!';
-    }
-    ?>
-    
+    <p><?php
+        if ($isOk) {
+            echo 'c\'est ok !!';
+        } else {
+            echo 'c\'est pas bon !!!';
+        }
+        ?></p>
 </body>
+
 </html>
