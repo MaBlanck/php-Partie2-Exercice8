0014df48a4fb8275f1e3ec4036f1ece779d4cba3
Max <maxblanck@outlook.fr>
2020-12-08 14:45:45 +0100

    php Partie 2  Exercice 8

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php 
+$isOk = 0;
+?>
+
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Exercice 8 Partie 2</title>
+</head>
+<body>
+    <h1>Exercice 8 Partie 2</h1>
+    <?php 
+    if ($isOk) {
+        echo 'c\'est ok !!';
+    } else {
+        echo 'c\'est pas bon !!!';
+    }
+    ?>
+    
+</body>
+</html>
